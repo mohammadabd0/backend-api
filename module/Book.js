9c2d780281c09e493edd3f8f49717f3a2903af3b
mohammadabd0 <mohammadabdo518@gmail.com>
2021-09-29 15:14:17 +0300

    update

--- module/Book.js
+++ module/Book.js
@@ -6,8 +6,8 @@ const bookSchema = new mongoose.Schema({
     title: String,
     description: String,
     status: String,
-    email: String,
-    ownerName: String
+    email: String
+   
 });
 const bookModel = mongoose.model('book', bookSchema);
 
@@ -17,31 +17,31 @@ function seedbookInformation() {
         title: 'java ',
         description: 'Java is a programming language and Java is fast, secure, and reliable. From laptops to datacenters, game consoles to scientific supercomputers, cell phones to the Internet, Java is everywhere! ',
         status: 'anything',
-        email: 'mohammadabd464@gmail.com',
-        ownerName: 'mohammad '
+        email: 'mohammadabdo518@gmail.com',
+    
     })
     const booktow = new bookModel({
         title: 'java script ',
         description: 'JavaScript is commonly used for creating web pages. It allows us to add dynamic behavior to the webpage and add special effects to the webpage. On websites, it is mainly used for validation purposes. JavaScript helps us to execute complex actions and also enables the interaction of websites with visitors. ',
         status: 'anything',
-        email: 'ahmahzz544@gmail.com',
-        ownerName: 'ahmah '
+        email: 'mohammadabdo518@gmail.com',
+        
     })
     const bookthree = new bookModel({
         title: 'c++ ',
         description: 'c++ is a programming language  is fast ',
         status: 'anything',
-        email: 'leen89@gmail.com',
-        ownerName: 'leen'
+        email: 'mohammadabdo518@gmail.com',
+      
     })
 
     bookone.save();
     booktow.save();
     bookthree.save();
 }
+// seedbookInformation();
 
 
 
-module.exports = {
-    bookModel
-}
+
+module.exports = bookModel
--- server.js
+++ server.js
@@ -9,26 +9,35 @@ const server = express();
 server.use(cors());
 const PORT = process.env.PORT;
 
+//Route
+server.get('/getbooks',getBookHandler)
+server.get('test'/test)
 mongoose.connect('mongodb://localhost:27017/books');
 
-//Route
-server.get('/getbooks',getBook)
 
 
 // getBook
-let bookModel = require('./module/Book')
-// localhost:3001/getBook?ownerName=mohammad
-function getBook(req,res){
-    let ownerName2 = req.query.ownerName
-    bookModel.find({ownerName:ownerName2},function(error,ownerData) {
+const bookModel = require('./module/Book')
+console.log('this is bookmodel',bookModel)
+// http://localhost:3001/getbooks?email=mohammad464@gmail.com
+function getBookHandler(req,res){
+    let email = req.query.email
+    bookModel.find({email},function(error,ownerData) {
         if(error) {
             console.log('error in getting data',error)
         } else {
             res.send(ownerData)
+            console.log(ownerData)
         }
     })
 }
 
+
+
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
 })
+
+function test(req,res){
+    res.send('test')
+}
